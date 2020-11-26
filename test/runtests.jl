070ceb6837cffad29da724d5036a3f0c05205aef
miroox <miroox@outlook.com>
2020-11-26 22:29:50 +0800

    test: just a placeholder
    such a test case seems nonsense

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using PhaseRetrieval
+using FFTW
+using Distances
 using Test
 
 @testset "PhaseRetrieval.jl" begin
-    # Write your tests here.
+    @test begin # see Gerchberg's original paper
+        c1 = [Int(abs(2t)<=1)*exp(im*15*pi*t^2) for t in -1:(1/256):(255/256)]
+        c2 = fft(c1)
+        r1, r2 = gerchbergsaxton(abs.(c1), abs.(c2), maxiter=130, init=c)
+        msd(c1, r1) < 0.01 && msd(c2, r2) < 0.01
+    end
 end
