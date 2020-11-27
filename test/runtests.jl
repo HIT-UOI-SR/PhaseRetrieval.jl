8674a29db42fa529c7c09845b6ca5c0e747239e1
miroox <miroox@outlook.com>
2020-11-27 19:57:37 +0800

    fix: fix typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
     @test begin # see Gerchberg's original paper
         c1 = [Int(abs(2t)<=1)*exp(im*15*pi*t^2) for t in -1:(1/256):(255/256)]
         c2 = fft(c1)
-        r1, r2 = gerchbergsaxton(abs.(c1), abs.(c2), maxiter=130, init=c)
+        r1, r2 = gerchbergsaxton(abs.(c1), abs.(c2), maxiter=130, init=c1)
         msd(c1, r1) < 0.01 && msd(c2, r2) < 0.01
     end
 end
